f4eaa59d295640db6e4d751f915ebbf3fd6c5b61
Raihansprt <rehannerrrrrrrrrrrr@gmail.com>
2022-01-26 11:33:03 +0700

    memisahkan navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,9 @@
+import Navitem from"./Navitem";
+
 export default function Nav( ){
     return (
-              <div className="nav flex text-white uppercase font-semibold tracking-widest text-center text-2xl">
-                <a href="#">profile</a>
-                <a href="#">skills</a>
-                <a href="#">project</a>
-                <a href="#">contact</a>
+              <div className="nav text-center">
+                <Navitem/>
               </div>
     );
-}
+    }
